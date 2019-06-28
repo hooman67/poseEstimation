f72ac91b146395dee9243945c3f303e833cd6499
hooman67 <houman_sh2001@hotmail.com>
2019-06-28 14:24:40 -0700

    error messages added. Unfortunaly multi ref frame averaging was accidently removed

---
+++ emptyDirs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm curves/*
+rm finalPlots/*
+rm finalVis/*
+rm finalVisBeforeRegisteration/*
+rm rejected_badRatio/*
+rm rejectedCurves/*
+rm rejected_notAllTeeth/*
+rm rejected_notEnoughLandmarks/*
---
+++ makeDirs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir curves
+mkdir finalPlots
+mkdir finalVis
+mkdir finalVisBeforeRegisteration
+mkdir rejected_badRatio
+mkdir rejectedCurves
+mkdir rejected_notAllTeeth
+mkdir rejected_notEnoughLandmarks
