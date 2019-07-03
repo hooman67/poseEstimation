2b5671618f1d594c634950e0183d75cb591aa0e6
hooman67 <houman_sh2001@hotmail.com>
2019-07-03 10:18:38 -0700

    before bugfix

--- makeDirs.sh
+++ makeDirs.sh
@@ -7,3 +7,11 @@ mkdir rejected_badRatio
 mkdir rejectedCurves
 mkdir rejected_notAllTeeth
 mkdir rejected_notEnoughLandmarks
+mkdir t1
+mkdir t2
+mkdir t3
+mkdir t4
+mkdir t5
+mkdir t6
+mkdir t7
+mkdir t8
